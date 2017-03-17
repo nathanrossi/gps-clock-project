2468a1476405d915f24e1e402755eac5acd85e97
Nathan Rossi <nathan@nathanrossi.com>
2017-03-17 22:51:45 +1000

    display-controller: Fix up HDL for corrector for functional synthesis
    Change the verilog for the color corrector so that it is clearly a set
    of ROMs. This makes it easy for yosys to synthesize.

--- display-controller/display-color-encoder.v
+++ display-controller/display-color-encoder.v
@@ -3,23 +3,10 @@ module display_color_encoder(clk, pixel, cpixel);
 	parameter segments = 1;
 	parameter cyclewidth = 8;
 	parameter bitwidth = 8;
-	parameter gamma = 2.2;
 
 	input wire clk;
 	input wire [((bitwidth * 3) * segments) - 1:0] pixel;
 	output reg [((bitwidth * 3) * segments) - 1:0] cpixel = 0;
-
-	integer i, j;
-	always @(posedge clk) begin
-		// connect the channels from corrected to rgb bits
-		for (j = 0; j < segments; j = j + 1) begin
-			for (i = 0; i < 3; i = i + 1) begin
-				// put the corrected pixel into buffer (to use BRAM)
-				cpixel[(((bitwidth) * 3) * j) + (bitwidth * i) +:bitwidth] <=
-					gamma_lookup[(3 * j) + i][pixel[(((bitwidth) * 3) * j) + (bitwidth * i) +:bitwidth]];
-			end
-		end
-	end
 
 	// Gamma Correction lookup table
 	//
@@ -37,18 +24,31 @@ module display_color_encoder(clk, pixel, cpixel);
 	// For now the pre-loading LUT info is based on all channels having the
 	// same gamma correction.
 	//
-	reg [cyclewidth - 1:0] gamma_lookup [0:(3 * segments) - 1][0:(2 ** bitwidth) - 1];
-	reg [cyclewidth - 1:0] gamma_lookup_data [0:(2 ** bitwidth) - 1];
-	initial begin
-		$readmemh("obj/gamma-lookup-table.hex", gamma_lookup_data);
-		for (i = 0; i < (2 ** bitwidth); i = i + 1) begin
-			for (j = 0; j < segments * 3; j = j + 1) begin
-				gamma_lookup[j][i] = gamma_lookup_data[i];
+
+	genvar g;
+	integer i;
+	generate
+		for (g = 0; g < segments * 3; g = g + 1) begin
+			reg [cyclewidth - 1:0] gamma_lookup_data [0:(2 ** bitwidth) - 1];
+			initial begin
+				$readmemh("obj/gamma-lookup-table.hex", gamma_lookup_data);
+				`ifndef SYNTHESIS
+				if (g == 0) begin
+					// Display the values, for manual checking
+					for (i = 0; i < (2 ** bitwidth); i = i + 1) begin
+						$display("[GAMMA LUT] in %d => %b [%d]", i, gamma_lookup_data[i], gamma_lookup_data[i]);
+					end
+				end
+				`endif
 			end
-			// Display the values, for manual checking
-			$display("[GAMMA LUT] in %b => %b [%d]", i, gamma_lookup[0][i], gamma_lookup[0][i]);
+
+			integer z;
+			always @(posedge clk) begin
+				z = g * bitwidth;
+				cpixel[z +:bitwidth] <= gamma_lookup_data[pixel[z +:bitwidth]];
+			end
 		end
-	end
+	endgenerate
 
 endmodule
 
