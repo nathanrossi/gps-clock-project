a2ff2863bc3314b41192345680c2fef65c8a352e
Nathan Rossi <nathan@nathanrossi.com>
2017-03-28 22:28:26 +1000

    display-controller/display-memory.v: Add segments parameter
    Extend the width of the in/out data values to be multiples of the
    segment count.

--- display-controller/display-memory.v
+++ display-controller/display-memory.v
@@ -1,5 +1,6 @@
 
 module display_memory(clk, flip, wen, wrow, wcol, rrow, rcol, wdata, rdata);
+	parameter segments = 1;
 	parameter rows = 8;
 	parameter columns = 32;
 	parameter width = 24;
@@ -8,16 +9,16 @@ module display_memory(clk, flip, wen, wrow, wcol, rrow, rcol, wdata, rdata);
 	input wire clk, flip, wen;
 	input wire [$clog2(rows)-1:0] wrow, rrow;
 	input wire [$clog2(columns)-1:0] wcol, rcol;
-	input wire [width-1:0] wdata;
-	output reg [width-1:0] rdata = {width{1'b0}};
+	input wire [(width * segments) - 1:0] wdata;
+	output reg [(width * segments) - 1:0] rdata = {(width * segments){1'b0}};
 
-	reg [width-1:0] memory[0:_addrwidth - 1];
+	reg [(width * segments) - 1:0] memory[0:_addrwidth - 1];
 	integer ik;
 	initial begin
-		$readmemh("obj/initial-image-memory.hex", memory);
-		//for (ik = 0; ik < _addrwidth - 1; ik = ik + 1) begin
-			//memory[ik] = 'h000f00; // set to high for now, so we get all leds
-		//end
+		//$readmemh("obj/initial-image-memory.hex", memory);
+		for (ik = 0; ik < _addrwidth - 1; ik = ik + 1) begin
+			memory[ik] = {(width * segments){1'b0}};
+		end
 	end
 
 	always @(posedge clk) begin
