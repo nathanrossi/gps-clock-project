0cc51ddb29b8d7a1e58cfdfa3c4193604243e47d
Nathan Rossi <nathan@nathanrossi.com>
2017-04-03 00:27:06 +1000

    display-controller: Improve and fix up some issues with spi-controller
    There was a SS bounce issue.

--- display-controller/spi-controller.v
+++ display-controller/spi-controller.v
@@ -14,9 +14,7 @@ module spi_controller(clk, rst, sclk, ss, mosi, miso, wdata, wen, wrow, wcol, re
 	output reg [$clog2(rows) - 1:0] wrow = 0;
 	output reg [$clog2(columns) - 1:0] wcol = 0;
 
-	reg integer row = 0;
 	reg integer column = 0;
-	reg integer segment = 0;
 	reg integer channel = 0;
 	output reg wen = 0;
 
@@ -39,23 +37,27 @@ module spi_controller(clk, rst, sclk, ss, mosi, miso, wdata, wen, wrow, wcol, re
 	wire [7:0] load_data;
 	wire load_valid;
 
+	reg [5:0] ss_buf;
+
 	always @(posedge clk) begin
 		if (rst == 1) begin
 			started <= 0;
 			loaded <= 0;
 			complete <= 0;
-			row <= 0;
 			column <= 0;
-			segment <= 0;
 			channel <= 0;
 			cmd <= 0;
 			wen <= 0;
+			wen_next <= 0;
 
 			last_ready <= 0;
 			mem_ready <= 0;
 		end else begin
 			// wen is low otherwise
 			wen <= 0;
+
+			// buffer the ss bits (TODO: move this to the spi-slave)
+			ss_buf <= {ss_buf[4:0], ss};
 
 			// check for frame flip, when ready goes 0 -> 1
 			if (ready == 1 && last_ready == 0) begin
@@ -65,19 +67,17 @@ module spi_controller(clk, rst, sclk, ss, mosi, miso, wdata, wen, wrow, wcol, re
 			end
 			last_ready <= ready;
 
-			if (!started) begin
+			if (started == 0) begin
 				// check command, needs to be 0xfx where x = row
 				if (load_valid == 1) begin
+					started <= 1;
 					if (load_data[7:4] == 4'hf) begin
 						// start of load
-						started <= 1;
-						segment <= 0;
 						channel <= 0;
 						column <= 0;
-						row <= load_data[3:0];
+						wrow <= load_data[3:0];
 						cmd <= _cmd_load_row;
 					end else if (load_data == 8'h10) begin
-						started <= 1;
 						cmd <= _cmd_end;
 					end else begin
 						cmd <= _cmd_invalid;
@@ -93,57 +93,43 @@ module spi_controller(clk, rst, sclk, ss, mosi, miso, wdata, wen, wrow, wcol, re
 				// the loaded signal is only high for one cycle, to tell the
 				// flipper that the memory was populated for flipping
 				loaded <= 0;
-			end else if (started) begin
+			end else if (started == 1) begin
 				case (cmd)
 					_cmd_load_row: begin
-						if (load_valid) begin
-							if (channel == 2) begin
+						// only load on valid and when not complete
+						if (load_valid && (complete == 0)) begin
+							// serial load into the wdata reg, dropping the oldest
+							wdata <= {wdata[(segments * bitwidth * 3) - bitwidth - 1:0], load_data};
+
+							if (channel == ((segments * 3) - 1)) begin
+								wcol <= column;
+								wen <= 1;
+
 								channel <= 0;
-								// loaded each channel, next segment
-								if (segment == segments - 1) begin
-									segment <= 0;
-									// loaded each segment, next column
-									if (column == columns - 1) begin
-										column <= 0;
-									end else begin
-										column <= column + 1;
-									end
-									wcol <= column;
-									wrow <= row;
-									wen <= (complete == 0);
+								// loaded each segment, next column
+								if (column == (columns - 1)) begin
+									column <= 0;
 								end else begin
-									segment <= segment + 1;
+									column <= column + 1;
 								end
 							end else begin
 								channel <= channel + 1;
 							end
-							// serial load into the wdata reg, dropping the oldest
-							// word
-							wdata <= {wdata[(segments * bitwidth * 3) - bitwidth - 1:0], load_data};
-							//wdata <= {load_data, load_data, load_data};
-						end else if (ss == 0) begin
-							started <= 0;
 						end
 					end
 					_cmd_end: begin
 						if (ss == 0) begin
-							started <= 0;
 							complete <= 1;
 							loaded <= (complete == 0);
-							row <= 0;
-							column <= 0;
-							segment <= 0;
-							channel <= 0;
-							wcol <= 0;
-							wrow <= 0;
-						end
-					end
-					_cmd_invalid: begin
-						if (ss == 0) begin
-							started <= 0;
 						end
 					end
 				endcase
+
+				// end of command
+				if (ss_buf == 6'b110000) begin
+					started <= 0;
+					cmd <= _cmd_invalid;
+				end
 			end
 		end
 	end
