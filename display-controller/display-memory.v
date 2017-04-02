869dca9d5ea13d79125fd25a6ec8f08fc0e6b16f
Nathan Rossi <nathan@nathanrossi.com>
2017-04-02 18:30:06 +1000

    display-controller/display-memory.v: Output rd/wr during testing

--- display-controller/display-memory.v
+++ display-controller/display-memory.v
@@ -12,16 +12,23 @@ module display_memory(clk, flip, wen, wrow, wcol, rrow, rcol, wdata, rdata);
 	output reg [(width * segments) - 1:0] rdata = {(width * segments){1'b0}};
 
 	reg [(width * segments) - 1:0] memory[0:(2 ** ($clog2(rows) + $clog2(columns) + 1)) - 1];
-	integer ik;
 	initial begin
+		integer ik;
 		for (ik = 0; ik < (2 ** ($clog2(rows) + $clog2(columns) + 1)) - 1; ik = ik + 1) begin
 			memory[ik] = {(width * segments){1'b0}};
 		end
 	end
 
 	always @(posedge clk) begin
-		if (wen)
+		if (wen) begin
+			`ifndef SYNTHESIS
+				$display("memory wr @ flip %d, wrow %2d, wcol %2d == %h", !flip, wrow, wcol, wdata);
+			`endif
 			memory[{!flip, wrow, wcol}] <= wdata;
+		end
+		`ifndef SYNTHESIS
+			$display("memory rd @ flip %d, rrow %2d, rcol %2d == %h", flip, rrow, rcol, memory[{flip, rrow, rcol}]);
+		`endif
 		rdata <= memory[{flip, rrow, rcol}];
 	end
 
