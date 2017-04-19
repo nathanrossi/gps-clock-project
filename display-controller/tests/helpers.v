789d9f91fc8b56a632fdf9263c8191c67486d7fd
Nathan Rossi <nathan@nathanrossi.com>
2017-04-19 23:26:32 +1000

    display-controller/tests/helpers.v: Improve and add more assert types

--- display-controller/tests/helpers.v
+++ display-controller/tests/helpers.v
@@ -1,20 +1,32 @@
 
-`define __gen_assert_eq(file, line, printtype, a0, a1) do begin \
-			if ((a0) != (a1)) begin \
-				$display("[@%8t %0s:%0d] assertion: 'a0' == 'a1' failed, printtype != printtype", $time, file, line, (a0), (a1)); \
+`define __gen_assert_compare(cmp, acmp, file, line, printtype, a0, a1) do begin \
+			if ((a0) acmp (a1)) begin \
+				$display("[@%8t %0s:%0d] assertion: 'a0' cmp 'a1' failed, printtype acmp printtype", $time, file, line, (a0), (a1)); \
 				$finish_and_return(1); \
 			end else begin \
-				$display("[@%8t %0s:%0d] assertion: 'a0' == 'a1', printtype == printtype", $time, file, line, (a0), (a1)); \
+				$display("[@%8t %0s:%0d] assertion: 'a0' cmp 'a1', printtype cmp printtype", $time, file, line, (a0), (a1)); \
 			end \
 		end while (0)
-
-`define assert_eq(actual, expected) do begin \
-			if ($size(expected) >= 4) begin \
-				`__gen_assert_eq(`__FILE__, `__LINE__, %h, actual, expected); \
+`define __gen_assert_vect_compare(cmp, acmp, file, line, a0, a1) do begin \
+			if ($size(a0) >= 4) begin \
+				`__gen_assert_compare(cmp, acmp, `__FILE__, `__LINE__, %h, a0, a1); \
 			end else begin \
-				`__gen_assert_eq(`__FILE__, `__LINE__, %b, actual, expected); \
+				`__gen_assert_compare(cmp, acmp, `__FILE__, `__LINE__, %b, a0, a1); \
 			end \
 		end while (0)
 
-`define assert_deq(actual, expected) `__gen_assert_eq(`__FILE__, `__LINE__, %0d, actual, expected)
+// defines for eq, ne, ge, gt, le, lt for vector and "d*" decimal
+`define assert_eq(actual, expected) `__gen_assert_vect_compare(==, !=, `__FILE__, `__LINE__, actual, expected)
+`define assert_ne(actual, expected) `__gen_assert_vect_compare(!=, ==, `__FILE__, `__LINE__, actual, expected)
+`define assert_ge(actual, expected) `__gen_assert_vect_compare(>=, <, `__FILE__, `__LINE__, actual, expected)
+`define assert_gt(actual, expected) `__gen_assert_vect_compare(>, <=, `__FILE__, `__LINE__, actual, expected)
+`define assert_le(actual, expected) `__gen_assert_vect_compare(<=, >, `__FILE__, `__LINE__, actual, expected)
+`define assert_lt(actual, expected) `__gen_assert_vect_compare(<, >=, `__FILE__, `__LINE__, actual, expected)
+
+`define assert_deq(actual, expected) `__gen_assert_compare(==, !=, `__FILE__, `__LINE__, %0d, actual, expected)
+`define assert_dne(actual, expected) `__gen_assert_compare(!=, ==, `__FILE__, `__LINE__, %0d, actual, expected)
+`define assert_dge(actual, expected) `__gen_assert_compare(>=, <, `__FILE__, `__LINE__, %0d, actual, expected)
+`define assert_dgt(actual, expected) `__gen_assert_compare(>, <=, `__FILE__, `__LINE__, %0d, actual, expected)
+`define assert_dle(actual, expected) `__gen_assert_compare(<=, >, `__FILE__, `__LINE__, %0d, actual, expected)
+`define assert_dlt(actual, expected) `__gen_assert_compare(<, >=, `__FILE__, `__LINE__, %0d, actual, expected)
 
