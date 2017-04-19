77c4c3b35f1a43bb9f691ee01959b2c3264138c1
Nathan Rossi <nathan@nathanrossi.com>
2017-04-19 20:10:28 +1000

    display-controller/tests/helpers.v: These were broken
    These were broken, and were not comparing full width of vectors/etc.
    This replaces the tasks which are a very limited language construct of
    verilog with pre-processor defines. Additionally assertions are always
    printed with "failed"/"passed" in the message.

--- display-controller/tests/helpers.v
+++ display-controller/tests/helpers.v
@@ -1,23 +1,20 @@
 
-`define assert_eq(a, b) helpers.assert_eq_info(`__FILE__, `__LINE__, a, b)
+`define __gen_assert_eq(file, line, printtype, a0, a1) do begin \
+			if ((a0) != (a1)) begin \
+				$display("[@%8t %0s:%0d] assertion: 'a0' == 'a1' failed, printtype != printtype", $time, file, line, (a0), (a1)); \
+				$finish_and_return(1); \
+			end else begin \
+				$display("[@%8t %0s:%0d] assertion: 'a0' == 'a1', printtype == printtype", $time, file, line, (a0), (a1)); \
+			end \
+		end while (0)
 
-module helpers;
-	task assert_eq(input a, b);
-		if (a != b) begin
-			$display("[%t] assertion: failed, (%d != %d)", $time, a, b);
-			$finish_and_return(1);
-		end
-	endtask
+`define assert_eq(actual, expected) do begin \
+			if ($size(expected) >= 4) begin \
+				`__gen_assert_eq(`__FILE__, `__LINE__, %h, actual, expected); \
+			end else begin \
+				`__gen_assert_eq(`__FILE__, `__LINE__, %b, actual, expected); \
+			end \
+		end while (0)
 
-	task assert_eq_info;
-		input [1024:0] file;
-		input integer line;
-		input a, b;
+`define assert_deq(actual, expected) `__gen_assert_eq(`__FILE__, `__LINE__, %0d, actual, expected)
 
-		if (a != b) begin
-			$display("[%8t %0s:%0d] assertion: failed, (%d != %d)", $time, file, line, a, b);
-			$finish_and_return(1);
-		end
-	endtask
-
-endmodule
