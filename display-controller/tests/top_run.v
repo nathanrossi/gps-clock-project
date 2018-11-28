8d43f77ca11d965ae08ea0c442c796a9fe1c9633
Nathan Rossi <nathan@nathanrossi.com>
2018-11-28 19:37:33 +1000

    display-controller/tests/top_run: Fix up test to run
    Fix up the test so that is triggers rst with a clock. Also fix up module
    dependencies.

--- display-controller/tests/top_run.v
+++ display-controller/tests/top_run.v
@@ -2,22 +2,29 @@
 
 module top_run;
 	reg clk = 0;
+	reg rst = 1;
 
 	top u_top (
-		.clk(clk)
+		.clk(clk),
+		.rst(rst)
 	);
 
 	// 5/5ns clock (10ns period)
 	//always
 		//# 5 clk = !clk;
 
-	integer i = 0;
 	initial begin
 		`setup_vcd(top_run);
+		repeat(32) begin
+			# 5 clk = !clk;
+			# 5 clk = !clk;
+			rst <= 1;
+		end
+		rst <= 0;
+
 		repeat(500000) begin
 			# 5 clk = !clk;
 			# 5 clk = !clk;
-			i = i + 1;
 		end
 		$finish(0);
 	end
