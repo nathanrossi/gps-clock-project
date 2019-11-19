537ba294814c6dfe8ea276ebe0db473c6a56f61b
Nathan Rossi <nathan@nathanrossi.com>
2019-11-19 20:33:12 +1000

    display-controller/spi_slave.v: Use recommended rst style/syntax
    Make sure rst condition is always last such that reset overrides any
    other logical condition.

--- display-controller/spi_slave.v
+++ display-controller/spi_slave.v
@@ -29,43 +29,43 @@ module spi_slave(clk, rst, sclk, ss, mosi, miso, data, valid);
 	// provides cleaner clocking and better signal reliablity. Clock must be
 	// 4x the system clk.
 	always @(posedge clk) begin
+		sclk_buf <= {sclk_buf[0], sclk};
+
 		if (rst == 1) begin
 			sclk_buf <= 2'b00;
-		end else begin
-			sclk_buf <= {sclk_buf[0], sclk};
 		end
 	end
 
 	always @(posedge clk) begin
+		valid <= 0;
+		if (ss == ss_active && sclk_buf == 2'b01) begin // rising_edge
+			// latch mosi
+			iword <= {iword[6:0], mosi};
+
+			count <= count + 1; // increment bit
+			if (count == 7) begin
+				valid <= 1;
+			end
+		end
+
 		if (rst == 1) begin
 			valid <= 0;
 			iword <= {8{1'b0}};
 			count <= 0;
-		end else begin
-			valid <= 0;
-			if (ss == ss_active && sclk_buf == 2'b01) begin // rising_edge
-				// latch mosi
-				iword <= {iword[6:0], mosi};
-
-				count <= count + 1; // increment bit
-				if (count == 7) begin
-					valid <= 1;
-				end
-			end
 		end
 	end
 
 	always @(posedge clk) begin
+		if (ss == ss_active && sclk_buf == 2'b10) begin // falling_edge
+			// latch miso
+			oword <= {oword[6:0], 1'b0};
+			if (count == 0) begin
+				oword <= iword;
+			end
+		end
+
 		if (rst == 1) begin
 			oword <= {8{1'b0}};
-		end else begin
-			if (ss == ss_active && sclk_buf == 2'b10) begin // falling_edge
-				// latch miso
-				oword <= {oword[6:0], 1'b0};
-				if (count == 0) begin
-					oword <= iword;
-				end
-			end
 		end
 	end
 
